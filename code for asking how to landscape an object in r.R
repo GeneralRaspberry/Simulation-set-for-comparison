866180cb66df974d0740788fdddb38e5d971208d
Thomas Taylor <t.h.taylor@outlook.com>
2021-09-30 14:30:22 +0100

    working code for a metric

--- code for asking how to landscape an object in r.R
+++ code for asking how to landscape an object in r.R
@@ -1,4 +1,4 @@
-library(tidyr)
+
 library(spatstat)
 
 set.seed(23)
@@ -13,6 +13,23 @@ coordinates<-coordinates[order(sim),]
 
 coord_list<-split(coordinates[,c("x","y")],coordinates$sim)
 ppplist<-lapply(coord_list,as.ppp,W=square(1000))
+
+kestlist<-list()
+MetricAverage<-c()
+for (i in (1:length(ppplist))){
+  kk<-Kest(ppplist[[i]])
+  kk_iso<-kk$iso
+  kk_pois<-kk$theo
+  
+  kk_div_na<-kk_iso/kk_pois
+  kk_div_0<-replace_na(kk_div_na,0)
+  kk_mean<-round(mean(kk_div_0),3)
+  MetricAverage<-c(MetricAverage,kk_mean)
+}
+
+
+
+
 
 
 
