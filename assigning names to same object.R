76c1547fd4caf4e45a4ae8409fa44c8ddab3d168
Thomas Taylor <t.h.taylor@outlook.com>
2021-10-11 14:53:15 +0100

    Add files via upload

--- assigning names to same object.R
+++ assigning names to same object.R
@@ -1,3 +1,8 @@
+library(tidyverse)
+library(plyr)
+
 assign(paste0("beta=",Figure2$beta," theta=",Figure2$theta,"metric=",
        Figure2$MetricAveragefinal),Figure2)
 
+masterdf<-list(mget(ls()))%>%reduce(full_join,by="theta")
+df<-ldply(masterdf,data.frame)
