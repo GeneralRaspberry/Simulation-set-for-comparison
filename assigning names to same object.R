07b31387f72f23652a578d7a0df5df5537798ff7
Thomas Taylor <t.h.taylor@outlook.com>
2021-10-11 15:21:29 +0100

    Add files via upload
    Included a plyr package that can form a table out of values, however this workflow is highly unstable.

--- assigning names to same object.R
+++ assigning names to same object.R
@@ -2,7 +2,9 @@ library(tidyverse)
 library(plyr)
 
 assign(paste0("beta=",Figure2$beta," theta=",Figure2$theta,"metric=",
-       Figure2$MetricAveragefinal),Figure2)
+       Figure2$MetricAveragefinal,"randmod=",Figure2$randmod),Figure2)
 
+rm(masterdf)
+rm(df)
 masterdf<-list(mget(ls()))%>%reduce(full_join,by="theta")
 df<-ldply(masterdf,data.frame)
