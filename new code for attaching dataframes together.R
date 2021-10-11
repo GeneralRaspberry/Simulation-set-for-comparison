2c4c9c315a94d3408e307c5b5f913b223e25d15e
Thomas Taylor <t.h.taylor@outlook.com>
2021-10-11 16:18:48 +0100

    Add files via upload

--- new code for attaching dataframes together.R
+++ new code for attaching dataframes together.R
@@ -1,8 +1,16 @@
 #########initial set up################################
 
 y<-Figure2
+
+##########################################################
+
+
+#Do not accidently press anything!
+
+
+
+
+names(Figure2)[names(Figure2)=="mean_r"]<-"mean_rsave"
 y<-rbind(y,Figure2)
 
 ##################rename if needed#########################
-
-names(Figure2)[names(Figure2)=="mean_r"]<-"mean_rsave"
