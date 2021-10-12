0f97fd8a930d603bdab1db29de5d3cdd9dd3545b
Thomas Taylor <t.h.taylor@outlook.com>
2021-10-12 14:04:54 +0100

    Add files via upload

--- code for heatmap.R
+++ code for heatmap.R
@@ -1,6 +1,21 @@
 library(ggplot2)
 
-ggplot(y,aes(x=(MetricAveragefinal),y=theta,fill=reldif)) +
+#################load dataframe with all values named y################
+
+########ensure all in correct order without duplicate####
+y<-arrange(y,randmod,theta)
+y<-distinct(y)
+
+
+#############then take average metric and include#################
+ymod<-y%>%
+  group_by(randmod)%>%
+  summarise_at(vars(MetricAveragefinal),list(name = mean))
+
+y$metricplot<-unlist(ymod[rep(seq_len(nrow(ymod)),each=10),2])
+
+#########now plot
+ggplot(y,aes(x=as.factor(metricplot),y=theta,fill=reldif)) +
   geom_tile() + 
   scale_fill_gradient(low="white",
                       high="darkred",
