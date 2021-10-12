5ef3608f93d528fc996c427878b6d4b850c4d4cc
Thomas Taylor <t.h.taylor@outlook.com>
2021-10-12 14:36:17 +0100

    Add files via upload

--- code for heatmap.R
+++ code for heatmap.R
@@ -12,11 +12,28 @@ ymod<-y%>%
   group_by(randmod)%>%
   summarise_at(vars(MetricAveragefinal),list(name = mean))
 
-y$metricplot<-unlist(ymod[rep(seq_len(nrow(ymod)),each=10),2])
+y$metricplot<-round(unlist(ymod[rep(seq_len(nrow(ymod)),each=10),2]),2)
 
-#########now plot
+
+############save plot####################
+
 ggplot(y,aes(x=as.factor(metricplot),y=theta,fill=reldif)) +
   geom_tile() + 
   scale_fill_gradient(low="white",
                       high="darkred",
-                      name="Relative Difference")
+                      name="Relative Difference")+
+  theme_minimal()+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
+  labs(x="Clustering Metric", y="\u03f4")
+
+ggheatmap1<-ggplot(y,aes(x=as.factor(metricplot),y=theta,fill=reldif)) +
+  geom_tile() + 
+  scale_fill_gradient(low="white",
+                      high="darkred",
+                      name="Relative Difference")+
+  theme_minimal()+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
+  labs(x="Clustering Metric", y="\u03f4")
+  
+
+ggsave("ggheatmap1.png", ggheatmap1,width=20, height=20, units = "cm")
